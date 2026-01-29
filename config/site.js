39ed383240d26c18156beb942a4afe3388882d34
Matt Blaschka <matt@dewclawarchery.com>
2026-01-29 09:33:19 -0800

    Formatting - closed notice

--- config/site.js
+++ config/site.js
@@ -1,4 +1,4 @@
 export const SHOW_HOLIDAY_BANNER = true;
 
 export const HOLIDAY_BANNER_TEXT =
-  "Our shop will be closed on Saturday, February 14, 2026.Regular hours resume Wednesday, February 18th.Thank you for understanding!";
+  "Our shop will be closed on Saturday, February 14, 2026. Regular hours resume Wednesday, February 18th. Thank you for understanding!";
