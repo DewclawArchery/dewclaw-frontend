ad707773d469ee834172580a9d73ba8a8a888a19
Matt Blaschka <matt@dewclawarchery.com>
2026-03-20 10:06:00 -0700

    Shop closed 4/18 banner

--- config/site.js
+++ config/site.js
@@ -1,4 +1,4 @@
-export const SHOW_HOLIDAY_BANNER = false;
+export const SHOW_HOLIDAY_BANNER = True;
 
 export const HOLIDAY_BANNER_TEXT =
-  "Our shop will be closed on Saturday, February 14, 2026. Regular hours resume Wednesday, February 18th. Thank you for understanding!";
+  "Our shop will be closed on Saturday, April 18, 2026. Regular hours resume Wednesday, April 22nd. Thank you for understanding!";
