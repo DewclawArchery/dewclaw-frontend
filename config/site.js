28a9c7eabfb06c68266aa708cc5531fb041b9cc2
Matt Blaschka <matt@dewclawarchery.com>
2026-03-20 10:29:08 -0700

    Fixed capitalization error in "true"

--- config/site.js
+++ config/site.js
@@ -1,4 +1,4 @@
-export const SHOW_HOLIDAY_BANNER = True;
+export const SHOW_HOLIDAY_BANNER = true;
 
 export const HOLIDAY_BANNER_TEXT =
   "Our shop will be closed on Saturday, April 18, 2026. Regular hours resume Wednesday, April 22nd. Thank you for understanding!";
