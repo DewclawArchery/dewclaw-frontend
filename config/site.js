13d16d9e3f54d7d16ca6844f829d37ca69ced4ff
Matt Blaschka <matt@dewclawarchery.com>
2026-06-04 10:08:10 -0700

    Add closed banner for father's day weekend.

--- config/site.js
+++ config/site.js
@@ -1,4 +1,4 @@
 export const SHOW_HOLIDAY_BANNER = false;
 
 export const HOLIDAY_BANNER_TEXT =
-  "Our shop will be closed on Saturday, April 18, 2026. Regular hours resume Wednesday, April 22nd. Thank you for understanding!";
+  "Our shop will be closed on Saturday, June 20th, 2026. Regular hours resume Wednesday, June 24th. Thank you for understanding!";
